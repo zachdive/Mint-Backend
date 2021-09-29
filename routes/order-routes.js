49edcbe9ee125a9781e361882529754e8e49eb5d
João Monteiro <jfcm.monteiro@gmail.com>
2021-09-29 15:00:47 +0100

    added order and cart routes

--- routes/order-routes.js
+++ routes/order-routes.js
@@ -9,4 +9,20 @@ router.get("/orders", async (req, res) => {
     }
 });
 
+router.post("/orders", async (req, res) => {
+    const {user, cart, schedule_delivery, total, status} = req.body;
+
+    if(!user || !cart || !schedule_delivery || !total || !status) {
+        res.status(400).message({message: "missing fields"});
+        return;
+    }
+    
+    try{
+        const response = await Order.create({user, cart, schedule_delivery, total, status});
+        res.status(200).json(response);
+    } catch(e) {
+        res.status(500).json({message: e.message});
+    }
+});
+
 module.exports = router;
